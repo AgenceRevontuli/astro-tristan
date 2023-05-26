06a382772c9e43b393690a905d00b9deefa9f579
AgenceRevontuli <tristan@revontuli.fr>
2023-05-26 11:11:33 +0200

    add scss and module folder

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Footer.module.scss'
+
+export default function Footer() {
+  return (
+    <footer>
+        <p>Tous droits réservés à Tristan Tornatore</p>
+    </footer>
+  )
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,18 @@
+import styles from '../styles/styles.module.scss'
+
 export default function Header() {
   return (
     <header>
-      
+      <div>
+        <p>Tristan Tornatore</p>  
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">Lien 1</a></li>
+          <li><a href="#">Lien 2</a></li>
+          <li><a href="#">Lien 3</a></li>
+        </ul>
+      </nav>
     </header>
   )
 }
